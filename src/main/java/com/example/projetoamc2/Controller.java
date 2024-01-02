28372af54765ed99db362183e3f28ee8eda14b75
martinha-ssv <marta.sofia.vasconcelos@gmail.com>
2024-01-02 19:26:00 +0000

    Extra files

--- src/main/java/com/example/projetoamc2/Controller.java
+++ src/main/java/com/example/projetoamc2/Controller.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
-    public BayesianNetwork Learn(String dataset_path, int n) {
+    public BayesianNetwork Learn(String dataset_path, int n, int iterations) {
         // Sofia :D
         return new BayesianNetwork(new Graph(1), new Sample(dataset_path), 0.5); // Escrevi esta linha só para não dar erro, depois muda :D
     }
